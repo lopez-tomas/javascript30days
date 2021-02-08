48ff8ba30e8f351b11971ceaa63bd849d574557d
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-02-08 03:35:56 -0300

    feat: drum kit logic completed

--- 01: JavaScript Drum Kit/main.js
+++ 01: JavaScript Drum Kit/main.js
@@ -1,18 +1,35 @@
-const getKeyDown = () => {
-	window.addEventListener('keydown', function(e){
+function removeTransition (e) {
+	if (e.propertyName !== 'transform') return // skip it if it's not a transform
+	this.classList.remove('playing')
+}
+
+const getAllKeys = () => {
+  const keys = document.querySelectorAll('.key')
+	keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+}
+
+const playSound = (e) => {
 		const keyCode = e.keyCode
-		const audio = getAudio(e.keyCode)
+		const audio = getAudio(keyCode)
+		const key = getKey(keyCode) 		
 		audio.currentTime = 0 // rewind to the start
 		audio.play()
-	})
+		key.classList.add('playing')
 }
 
 const getAudio = (keyCode) => {
 	const audio = document.querySelector(`audio[data-key="${keyCode}"]`)
 	if (!audio) return // stop the function from running all together
 	else return audio
-};
+}
+
+const getKey = (keyCode) => {
+	const key = document.querySelector(`.key[data-key="${keyCode}"]`)
+	if (!key) return // stop the function from running all together
+	else return key
+}
 
 window.onload = () => {
-	getKeyDown()
+	getAllKeys()	
+	window.addEventListener('keydown', playSound)
 }
